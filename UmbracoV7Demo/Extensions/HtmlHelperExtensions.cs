a473d92733480d0a61835246521da529809cf5ab
Chen Lu <chenlu.design@gmail.com>
2014-08-11 10:18:51 +0100

    fixed RSS feed and News paging rewrite issue

---
+++ UmbracoV7Demo/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace UmbracoV7Demo.Extensions
+{
+    using System.Web.Mvc;
+    using umbraco;
+
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString Truncate(this HtmlHelper html, string value, int count, string elipses)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return MvcHtmlString.Empty;
+            }
+
+            var cleanHtml = library.StripHtml(value);
+
+            return cleanHtml.Length <= count ? MvcHtmlString.Create(cleanHtml) : MvcHtmlString.Create(cleanHtml.Substring(0, count - 1) + elipses);
+        }
+    }
+}
