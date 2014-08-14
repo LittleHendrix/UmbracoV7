3f4375fec74cdcbc12ca82f9a640ea91c9d2140e
Chen Lu <chenlu.design@gmail.com>
2014-08-14 17:32:28 +0100

    started building site search

---
+++ UmbracoV7Demo/Controllers/SearchController.cs
@@ -0,0 +1,130 @@
+﻿namespace UmbracoV7Demo.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using Examine;
+    using Examine.Providers;
+    using Examine.SearchCriteria;
+
+    using Umbraco.Web.Models;
+    using Umbraco.Web.Mvc;
+
+    using UmbracoV7Demo.ViewModels;
+
+    public class SearchController : RenderMvcController
+    {
+        public override ActionResult Index(RenderModel umbracoModel)
+        {
+            return this.GetDefaultAction(umbracoModel);
+        }
+
+        public ActionResult Search(RenderModel umbracoModel, string searchTerms = "", string json = "")
+        {
+            if (!string.IsNullOrEmpty(json))
+            {
+                var jsonResults =
+                    this.GetSearchResults(searchTerms, 100)
+                        .Results.Select(x => new { Url = this.Umbraco.NiceUrl(x.Id), Title = x.Fields["nodeName"] });
+
+                return this.Json(jsonResults);
+            }
+
+            SearchResultsViewModel model = this.GetSearchResults(searchTerms, 20);
+
+            return this.GetDefaultAction(model);
+        }
+
+        private ActionResult GetDefaultAction(object model)
+        {
+            // get the template name from the route values:
+            // string template = this.ControllerContext.RouteData.Values["action"].ToString();
+
+            // return this.View(template, model);
+
+            return this.CurrentTemplate(model);
+        }
+
+        private SearchResultsViewModel GetSearchResults(string searchTerms, int pageSize)
+        {
+            int page;
+            int resultCount = 0;
+            int totalPages = 0;
+            IOrderedEnumerable<SearchResult> searchResults = null;
+
+            if (!int.TryParse(this.Request.QueryString["Page"], out page))
+            {
+                page = 1;
+            }
+
+            if (!string.IsNullOrEmpty(searchTerms))
+            {
+                searchTerms = searchTerms.ToLower();
+                BaseSearchProvider searchProvider = ExamineManager.Instance.SearchProviderCollection["ExternalSearcher"];
+                ISearchCriteria searchCriteria = searchProvider.CreateSearchCriteria(BooleanOperation.Or);
+                IBooleanOperation queryBuilder = null;
+
+                foreach (var searchValue in searchTerms.Split(' ').Where(searchTerm => !string.IsNullOrEmpty(searchTerm.RemoveStopWords())))
+                {
+                    if (queryBuilder == null)
+                    {
+                        queryBuilder =
+                            searchCriteria.GroupedOr(
+                                new[] { "nodeName", "metaTitle", "metaDescription", "bodyText" },
+                                searchValue);
+                    }
+                    else
+                    {
+                        queryBuilder =
+                            queryBuilder.Or()
+                                .GroupedOr(
+                                    new[] { "nodeName", "metaTitle", "metaDescription", "bodyText" },
+                                    searchValue);
+                    }
+                }
+
+                if (queryBuilder != null)
+                {
+                    queryBuilder = queryBuilder.Not().Field("template", "0");
+
+                    ISearchCriteria query =
+                        queryBuilder.Not()
+                            .GroupedOr(new[] { "umbracoNaviHide", "nodeTypeAlias" }, new[] { "1", "Folder" })
+                            .Compile();
+
+                    ISearchResults results = searchProvider.Search(query);
+                    searchResults = results.OrderByDescending(x => x.Score);
+                    resultCount = results.TotalItemCount;
+                }
+
+                totalPages = (int)Math.Ceiling(resultCount / (double)pageSize);
+
+                if (page > totalPages)
+                {
+                    page = totalPages;
+                }
+                else if (page < 1)
+                {
+                    page = 1;
+                }
+            }
+
+            var searchResultModel = new SearchResultsViewModel
+            {
+                Page = page,
+                PageSize = pageSize,
+                ResultCount = resultCount,
+                TotalPages = totalPages,
+                SearchTerms = searchTerms
+            };
+
+            if (searchResults != null)
+            {
+                searchResultModel.Results = searchResults.Skip((page - 1) * pageSize).Take(pageSize);
+            }
+
+            return searchResultModel;
+        }
+    }
+}
---
+++ UmbracoV7Demo/ViewModels/SearchResultsViewModel.cs
@@ -0,0 +1,68 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SearchResultsViewModel.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The search results view model.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace UmbracoV7Demo.ViewModels
+{
+    using System.Collections.Generic;
+
+    using Examine;
+
+    using Umbraco.Web;
+
+    /// <summary>
+    ///     The search results view model.
+    /// </summary>
+    public class SearchResultsViewModel
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="SearchResultsViewModel" /> class.
+        /// </summary>
+        public SearchResultsViewModel()
+        {
+            this.Results = new List<SearchResult>();
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        ///     Gets or sets the page.
+        /// </summary>
+        public int Page { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the page size.
+        /// </summary>
+        public int PageSize { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the result count.
+        /// </summary>
+        public int ResultCount { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the results.
+        /// </summary>
+        public IEnumerable<SearchResult> Results { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the search terms.
+        /// </summary>
+        public string SearchTerms { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the total pages.
+        /// </summary>
+        public int TotalPages { get; set; }
+
+        #endregion
+    }
+}
