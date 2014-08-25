d261405b50339c24129d702c81c14a517a39dacd
Chen Lu <chenlu.design@gmail.com>
2014-08-25 10:50:01 +0100

    added IRepository and IUnitOfWork

---
+++ UmbracoV7Demo/Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IUnitOfWork.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The UnitOfWork interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace UmbracoV7Demo.Core.Interfaces
+{
+    using System;
+
+    /// <summary>
+    /// The UnitOfWork interface.
+    /// </summary>
+    internal interface IUnitOfWork : IDisposable
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The commit.
+        /// </summary>
+        void Commit();
+
+        /// <summary>
+        /// The rollback.
+        /// </summary>
+        void Rollback();
+
+        #endregion
+    }
+}
