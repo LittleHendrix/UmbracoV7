0d5904c2b13d302b7307d32ac5875ed67ae936c1
Chen Lu <chenlu.design@gmail.com>
2014-09-04 15:26:28 +0100

    add IPaged interface and PageResult class

--- UmbracoV7Demo/Core/Interfaces/IPaged.cs
+++ UmbracoV7Demo/Core/Interfaces/IPaged.cs
@@ -1,48 +1,11 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="IPaged.cs" company="">
-//   
-// </copyright>
-// <summary>
-//   The Paged interface.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
-
-namespace UmbracoV7Demo.Core.Interfaces
+﻿namespace UmbracoV7Demo.Core.Interfaces
 {
     using System.Collections.Generic;
 
-    /// <summary>
-    /// The Paged interface.
-    /// </summary>
-    /// <typeparam name="T">
-    /// </typeparam>
     public interface IPaged<T> : IEnumerable<T>
     {
-        #region Public Properties
-
-        /// <summary>
-        /// Gets the count.
-        /// </summary>
         int Count { get; }
 
-        #endregion
-
-        #region Public Methods and Operators
-
-        /// <summary>
-        /// The get range.
-        /// </summary>
-        /// <param name="index">
-        /// The index.
-        /// </param>
-        /// <param name="count">
-        /// The count.
-        /// </param>
-        /// <returns>
-        /// The <see cref="IEnumerable"/>.
-        /// </returns>
         IEnumerable<T> GetRange(int index, int count);
-
-        #endregion
     }
 }
---
+++ UmbracoV7Demo/Infrastructure/Util/PagedResult.cs
@@ -0,0 +1,41 @@
+﻿namespace UmbracoV7Demo.Infrastructure.Util
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using UmbracoV7Demo.Core.Interfaces;
+
+    public class PagedResult<T> : IPaged<T>
+    {
+        private readonly IQueryable<T> source;
+
+        public PagedResult(IQueryable<T> source)
+        {
+            this.source = source;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.source.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.source.Count();
+            }
+        }
+
+        public IEnumerable<T> GetRange(int index, int count)
+        {
+            return this.source.Skip(index).Take(count);
+        }
+    }
+}
